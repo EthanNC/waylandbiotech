8f03a3e6dfd0e95bd4638c1b93e471330c9e8f0c
EthanNC <Ethan_Cumming@student.uml.edu>
2020-06-21 14:27:24 -0400

    updated SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,19 @@
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = 'https://www.waylandbio.com',
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env
+const isNetlifyProduction = NETLIFY_ENV === 'production'
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL
+
 module.exports = {
   siteMetadata: {
     title: `Wayland Biotherapeutics`,
     description: `Novel Therapeutics for Covid-19 and Cancer Using Receptor Decoys`,
     author: `@EthanNC`,
+    siteUrl
+
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -30,5 +41,27 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     "gatsby-plugin-postcss",
+    `gatsby-plugin-advanced-sitemap`,
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: '*' }],
+          },
+          'branch-deploy': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+          'deploy-preview': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+        },
+      },
+    },
   ],
 }
